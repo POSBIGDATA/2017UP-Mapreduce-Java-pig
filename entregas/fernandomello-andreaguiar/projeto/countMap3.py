eedc30e99eb0c253d1f8c06ec31364fa27e2b41c
a3w <cadastros@a3w.com.br>
2017-05-20 17:32:45 -0300

    Update countMap3.py
    a biblioteca NLTK passou a ser executada no reduce e não no map

--- entregas/fernandomello-andreaguiar/projeto/countMap3.py
+++ entregas/fernandomello-andreaguiar/projeto/countMap3.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 import sys
 import re
-from nltk.corpus import wordnet as wn
-from nltk import pos_tag
-import nltk
-
 
 for line in sys.stdin:
 	line = re.sub(re.compile('([^A-Z a-z])+'), ' ', line)
-	words = nltk.word_tokenize(line)
-	classificacao = pos_tag(words)
-	for word in classificacao:
-		#removendo as preposicoes do texto
-		if (word[1] != 'IN'):
-			print (word[0].upper() + '\t1')	
+	words = line.strip().split()
+	for word in words:
+		print (word.upper() + '\t1')
